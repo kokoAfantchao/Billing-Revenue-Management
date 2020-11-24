f9d22870e03e7444237ad4dd04b8acc169926f5f
Nestor Afantchao <nextybrown@users.noreply.github.com>
2020-11-24 01:45:45 -0500

    route config and dash bord

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   # below the  front-end path config
   get 'patients/board'
   get 'dashboard', to: 'dashboard#board'
+  get '/', to: 'dashboard#board'
 
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
